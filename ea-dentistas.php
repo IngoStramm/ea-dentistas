090e611157c0830e81a7c1790a904e43c3ac3ecf
Ingo Stramm <ingostramm@gmail.com>
2023-11-09 15:35:14 -0300

    new version

--- ea-dentistas.php
+++ ea-dentistas.php
@@ -4,7 +4,7 @@
  * Plugin Name: EA Dentistas
  * Plugin URI: https://agencialaf.com
  * Description: Descrição do EA Dentistas.
- * Version: 0.0.4
+ * Version: 0.0.5
  * Author: Ingo Stramm
  * Text Domain: ea-dentistas
  * License: GPLv2
