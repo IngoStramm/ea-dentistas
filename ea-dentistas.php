89e441dc8515235f8420085f891a86bd943bbc38
Ingo Stramm <ingostramm@gmail.com>
2024-02-08 16:22:31 -0300

    update script version

--- ea-dentistas.php
+++ ea-dentistas.php
@@ -4,7 +4,7 @@
  * Plugin Name: EA Dentistas
  * Plugin URI: https://agencialaf.com
  * Description: Descrição do EA Dentistas.
- * Version: 0.0.25
+ * Version: 0.0.26
  * Author: Ingo Stramm
  * Text Domain: ea-dentistas
  * License: GPLv2
--- scripts.php
+++ scripts.php
@@ -51,7 +51,7 @@ function ea_dentistas_admin_scripts()
     if (!is_user_logged_in())
         return;
 
-    $version = '1.0.6';
+    $version = '1.0.7';
 
     $min = (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '10.0.0.3'))) ? '' : '.min';
 
